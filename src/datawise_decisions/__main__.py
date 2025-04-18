87a6d0563d3396ce1498afe85c9fd6f426832446
emilcode <ephraim.eckl@posteo.de>
2025-04-18 12:12:00 +0000

    [added] Initial structure of project with pyproject.toml

---
+++ src/datawise_decisions/__main__.py
@@ -0,0 +1,8 @@
+import sys
+from datawise_decisions import seer
+
+def main():
+    seer.see_rul(sys.argv[1], sys.argv[2])
+
+if __name__ == "__main__":
+    main()
---
+++ src/datawise_decisions/seer.py
@@ -0,0 +1,12 @@
+def see_soh():
+    """
+    State of Health (SoH) function placeholder.
+    """
+    pass
+
+def see_rul(a = 5, b = 4):
+    """
+    Remaining Useful Live (RUL) function placeholder.
+    """
+    print(int(a) + int(b))
+    return
